a2a2e9732b5aebd3e3939c146c12d3603901651a
Mac Lee <seemin0506@gmail.com>
2024-07-04 21:09:46 -0700

    Add functionality to NavBar, able to navigate through sections by clicking the relevant text. The texts bold and add a dot at the end to indicate active section. Need to add change to dynamically change as the user scrolls. Minimal style changes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,36 @@
-import Link from "next/link";
+import NavBar from "@/components/NavBar/NavBar";
+import SkillsIndex from "@/components/SkillsIndex";
 
 export default function Page() {
     return (
-        <div className="flex flex-col md:flex-row items-center md:justify-evenly md:items-stretch bg-violet-300 px-0 md:px-2 ">
-            <header className="sticky top-0 md:h-screen flex flex-col justify-between gap-8 bg-red-400 w-11/12 md:w-5/12 min-w-min py-10 md:py-20 mb-14 md:mb-0">
-                <div className="flex flex-col justify-between gap-8">
+        <div className="flex flex-col md:flex-row items-center md:justify-evenly md:items-stretch px-0 md:px-2">
+            <header className="md:sticky md:top-0 md:h-screen flex flex-col justify-between gap-6 w-11/12 md:w-5/12 2xl:w-1/3 min-w-min pt-10 md:pt-20 2xl:pt-24">
+                <div className="flex flex-col justify-between gap-10">
                     <span className="text-nowrap">
-                        <h1 className="text-4xl font-bold">Mac Lee</h1>
-                        <h2 className="font-medium">Junior Software Developer</h2>
+                        <h1 className="text-4xl md:text-6xl font-bold">Mac Lee</h1>
+                        <h2 className="font-semibold md:text-xl">Junior Software Developer</h2>
                     </span>
-
-                    <nav>
-                        <ul className="list-none font-medium text-sm" >
-                            <li className="my-2" ><Link href="/">About</Link></li>
-                            <li className="my-2" ><Link href="/">Projects</Link></li>
-                            <li className="my-2" ><Link href="/">Contact</Link></li>
-                        </ul>
-                    </nav>
+                    <NavBar />
                 </div>
-                
-                <div className="bg-blue-500 flex flex-wrap">
-                    <div className="bg-slate-200 px-2 rounded-full w-fit mr-1 mb-1">
-                        <span className="text-sm text-slate-700 font-bold" >React</span>
-                    </div>
-                    <div className="bg-slate-200 px-2 rounded-full w-fit mr-1 mb-1">
-                        <span className="bg-slate-200 rounded-full text-sm text-slate-700 font-bold text-nowrap" >React Native</span>
-                    </div>
-                    <div className="bg-slate-200 px-2 rounded-full w-fit mr-1 mb-1">
-                        <span className="bg-slate-200 rounded-full text-sm text-slate-700 font-bold" >Node.js</span>
-                    </div>
-                    <div className="bg-slate-200 px-2 rounded-full w-fit mr-1 mb-1">
-                        <span className="bg-slate-200 rounded-full text-sm text-slate-700 font-bold" >Next.js</span>
-                    </div>
-                    <div className="bg-slate-200 px-2 rounded-full w-fit mr-1 mb-1">
-                        <span className="bg-slate-200 rounded-full text-sm text-slate-700 font-bold" >Express</span>
-                    </div>
-                    <div className="bg-slate-200 px-2 rounded-full w-fit mr-1 mb-1">
-                        <span className="bg-slate-200 rounded-full text-sm text-slate-700 font-bold" >TypeScript</span>
-                    </div>
-                    <div className="bg-slate-200 px-2 rounded-full w-fit mr-1 mb-1">
-                        <span className="bg-slate-200 rounded-full text-sm text-slate-700 font-bold" >JavaScript</span>
-                    </div>
-                    <div className="bg-slate-200 px-2 rounded-full w-fit mr-1 mb-1">
-                        <span className="bg-slate-200 rounded-full text-sm text-slate-700 font-bold" >C#</span>
-                    </div>
-                    <div className="bg-slate-200 px-2 rounded-full w-fit mr-1 mb-1">
-                        <span className="bg-slate-200 rounded-full text-sm text-slate-700 font-bold" >PostgreSQL</span>
-                    </div>
-                    <div className="bg-slate-200 px-2 rounded-full w-fit mr-1 mb-1">
-                        <span className="bg-slate-200 rounded-full text-sm text-slate-700 font-bold" >HTML/CSS</span>
-                    </div>
-                </div>
+                <SkillsIndex />
             </header>
 
-            <main className="bg-green-500 min-w-min w-11/12 md:w-5/12 md:pt-20 ">
-                <h1>About</h1>
-                <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-                <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-                <h1>Experience</h1>
-                <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-                <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-                <h1>Contact</h1>
+            <main className="min-w-min w-11/12 md:w-5/12 2xl:w-1/3 md:pt-20 2xl:pt-24">
+                <div>
+                    <h1 id="about" >About</h1>
+                    <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+                    <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+                </div>
+                <div>
+                    <h1 id="projects" >Projects</h1>
+                    <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+                    <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+                </div>
+                <div>
+                    <h1 id="contact" >Contact</h1>
+                    <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+                    <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+                </div>
             </main>
         </div>
     );
