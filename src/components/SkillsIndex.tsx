b5d3f32259d120912d26f98388843bb09ee61b1d
Mac Lee <seemin0506@gmail.com>
2024-07-04 21:07:46 -0700

    Add skil index, at the bottom of the header section with all my lsited skills. Reduce redundancy in code by using map function and a list of all my skills.

---
+++ src/components/SkillsIndex.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const SkillsIndex : React.FC = () => {
+    const mySkills = [
+        "React",
+        "React Native",
+        "Node.js",
+        "Express",
+        "Next.js",
+        "TypeScript",
+        "JavaScript",
+        "C#",
+        "PostgreSQL",
+        "HTML/CSS"
+    ];
+    
+    return(
+        <div className="flex flex-wrap sticky top-0 mb-20 2xl:mb-44 select-none">
+            {mySkills.map((value) => 
+                <div className="bg-slate-300 flex items-center justify-center px-2 py-1 rounded-full w-fit mr-1 mb-1">
+                    <span className="text-xs md:text-sm text-slate-700 font-bold text-nowrap" >{value}</span>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default SkillsIndex;
