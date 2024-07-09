e42ca0a9360ceb032dc426fa85a9068fca2a3858
Mac Lee <seemin0506@gmail.com>
2024-07-09 16:37:35 -0700

    Add Footer component. Empty paragraph element.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React, {
+    FC,
+} from "react";
+
+const Footer : FC = () => {
+    return(
+        <div className="mb-5">
+            <p>qwerty</p>
+        </div>
+    );
+};
+
+export default Footer;
