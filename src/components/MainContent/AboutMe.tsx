c074b915849a153dfa2a1efb10b9919f484a0155
Mac Lee <seemin0506@gmail.com>
2024-10-19 03:52:33 -0700

    Add AboutMe component.

---
+++ src/components/MainContent/AboutMe.tsx
@@ -0,0 +1,7 @@
+export default () => {
+    return(
+        <div>
+            <p>Hello! I'm Mac, a dedicated developer with a passion for creating innovative solutions with elegant code. I’m always eager to learn new technologies and collaborate with talented individuals. I hope we can work and grow together to build something great!</p>
+        </div>
+    );
+};
